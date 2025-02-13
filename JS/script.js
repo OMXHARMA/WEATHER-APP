fa9c9701cf50f99ce7e38b33dbe98d4da349cd21
omsharma <omxrma@icloud.com>
2025-02-13 19:07:18 +0530

    simple javascript coad

---
+++ JS/script.js
@@ -0,0 +1,5 @@
+document.getElementById('search-btn').addEventListener('click',
+    function() {
+        const city = document.getElementById('city-input').value;
+        console.log('City entered:', city);
+});
